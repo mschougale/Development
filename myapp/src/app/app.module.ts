c3a4c89d925a7f3c54ede0dda28e083cec8f571c
mschougale <maheshchougale19@gmail.com>
2018-01-11 07:20:43 +0530

    added employee component

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {EmployeeComponent } from './Employee/employee.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,EmployeeComponent
   ],
   imports: [
     BrowserModule
