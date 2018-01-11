05d083fb849a5a1bb466c75814c4a8a976548b8d
mahesh_chougale <mahesh.chougale@blazeclan.com>
2018-01-11 12:53:03 +0530

    add validation on login form

--- myapp/src/app/User/user.component.ts
+++ myapp/src/app/User/user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder,Validators  } from '@angular/forms';
+
 
 @Component({
   selector: 'main-app',
@@ -7,5 +9,18 @@ import { Component } from '@angular/core';
 })
 
 export class userComponent {
+  loginForm : FormGroup;
 
+  constructor(fb: FormBuilder){
+    this.loginForm = fb.group({
+      'username':[null, Validators.required],
+      'remember':false,
+      'password':[null, Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(15)])],
+    })
+  }
+  
+  submitForm(value: any){
+    console.log(value);
+    alert(value.username);
+  }
 }
--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import {userComponent } from './User/user.component';
+import { userComponent } from './User/user.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,userComponent
+    AppComponent, userComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
